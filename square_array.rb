5818f10b3a13fff90493e08f3ff3a373b52c9f12
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-28 01:06:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here	  # your code here
   array2 = []
-array.each do |element|
-    array2 << element ** 2 
+array.each do |i|
+    array2 << i ** 2 
   end 
 
   array2
