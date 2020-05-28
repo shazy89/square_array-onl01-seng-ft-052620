72b1c4486f598008645827c2a216ac9fbfd4c34d
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-28 01:04:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,10 @@
 def square_array(array)
-  # your code here	  new_array = []
-  array.each { |n| new_array << (n * n) }
-  new_array
-end 	end 
+  # your code here	  # your code here
+  array2 = []
+
+  array.each do |element|
+    array2 << element ** 2 
+  end 
+
+  array2
+end 	
